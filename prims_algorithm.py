7068db9d6e4a4736a0528ca8bb9ca859b065341c
YonghwanYim <uko991531@gmail.com>
2023-10-18 13:23:12 +0900

    Initial commit

---
+++ prims_algorithm.py
@@ -0,0 +1,17 @@
+"""
+* Title : Prim's algorithm using binary heap structure.
+* Author : Yonghwan Yim
+* Final update : 2023.10.18
+"""
+
+MAX_V = 100
+INF = 987654321
+# Number of node
+V = 10
+
+def prims():
+    print(INF)
+
+
+
+
